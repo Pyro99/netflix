41fd99468894741f98a57a4603ddef23f2d3cc9e
Utkarsh <utkarshgunner10@gmail.com>
2023-01-01 18:56:24 +0530

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDxuxzoxiRPJcu5FdsDd4vdR1wS9gg_0_s",
+  authDomain: "netflix-df14a.firebaseapp.com",
+  projectId: "netflix-df14a",
+  storageBucket: "netflix-df14a.appspot.com",
+  messagingSenderId: "584917936639",
+  appId: "1:584917936639:web:be7b3cf5e09101c538dfca",
+};
